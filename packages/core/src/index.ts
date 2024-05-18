7d00c910247a3cfcdb6eeb38ba93416ba6052833
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-05-18 13:38:34 -0400

    chore(core): setup vitest

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,2 @@
-export * from "./Travel";
-export * from "./types";
+export * from "./legacy/Travel";
+export * from "./legacy/types";
---
+++ packages/core/src/sample.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+
+describe("test", () => {
+  it("should test", () => {
+    expect(true).toBe(true);
+  });
+});
