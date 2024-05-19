55c92e397b030fbdc4d16e37708863e705ff3baa
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-05-18 22:21:02 -0400

    f

--- packages/core/src/Nodes/Token.ts
+++ packages/core/src/Nodes/Token.ts
@@ -1,5 +1,7 @@
-import { UniDeclarator, UniOperation, UniProvider } from "../legacy/types";
+import { IDeclarator } from "./Declarator";
+import { IOperation } from "./Operation";
+import { IProvider } from "./Provider";
 import { IType } from "./Types";
-import { IView } from "./View/NodeView";
+import { IView } from "./View";
 
-export type IToken = IType | UniOperation | UniDeclarator | UniProvider | IView;
+export type IToken = IType | IOperation | IDeclarator | IProvider | IView;
