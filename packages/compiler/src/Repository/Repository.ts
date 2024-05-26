d6a77448c1ed59d161ca8a740ee78b27ed6eab7f
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-05-25 21:40:08 -0400

    feat(compiler): UniRepository

---
+++ packages/compiler/src/Repository/Repository.ts
@@ -0,0 +1,29 @@
+import { IUniNode, VisitAllNodes } from "@unispec/core";
+
+type IRepositoryEntrypoint = IUniNode | Iterable<IUniNode> | undefined | null;
+
+export class UniRepository {
+  #nodes = new Set<IUniNode>();
+
+  *getNodes(): Iterable<IUniNode> {
+    yield* this.#nodes;
+  }
+
+  get Nodes(): Iterable<IUniNode> {
+    return this.getNodes();
+  }
+
+  constructor(entrypoint?: IRepositoryEntrypoint) {
+    this.Add(entrypoint);
+  }
+
+  Add(entrypoint: IRepositoryEntrypoint): this {
+    if (entrypoint) {
+      for (const node of VisitAllNodes(entrypoint)) {
+        this.#nodes.add(node);
+      }
+    }
+
+    return this;
+  }
+}
