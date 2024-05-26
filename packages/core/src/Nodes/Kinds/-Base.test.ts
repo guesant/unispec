ccc05652c86719f2eb044befda0e1475cc5e8875
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-05-25 20:26:55 -0400

    fix: imports

--- packages/core/src/Nodes/Kinds/-Base.test.ts
+++ packages/core/src/Nodes/Kinds/-Base.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { IUniNodeBase, UniNodeBase } from "./-Base";
+import { IUniNodeBase, IUniNodeTypeBase, UniNodeBase, UniNodeTypeBase } from "./-Base";
 
 const fixtures = {
   UniNodeTypeBase: {
