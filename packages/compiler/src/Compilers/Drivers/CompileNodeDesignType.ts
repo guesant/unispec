e34f7f600dba231abbe58735d60eb23ad11764d3
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-05-26 10:17:29 -0400

    feat(compiler): CompileNodeDesignType

---
+++ packages/compiler/src/Compilers/Drivers/CompileNodeDesignType.ts
@@ -0,0 +1,41 @@
+import { IUniNodeTypeString, IUniNodeView } from "@unispec/core";
+import { CompileNode } from "../Node/CompileNode";
+
+export class CompileNodeDesignType extends CompileNode {
+  HandleTypeString(node: IUniNodeTypeString) {
+    switch (node.format) {
+      case "date-time": {
+        return Date;
+      }
+
+      case "uuid":
+      case "e-mail":
+      case "time":
+      case "date":
+      default: {
+        return String;
+      }
+    }
+  }
+
+  HandleTypeInteger(): unknown {
+    return Number;
+  }
+
+  HandleTypeBoolean(): unknown {
+    return Boolean;
+  }
+
+  HandleView(node: IUniNodeView) {
+    if (node.partialOf) {
+      const dereferenced = this.repository.GetRealTarget(node);
+
+      if (dereferenced) {
+        return this.Handle(dereferenced);
+      }
+    }
+
+    const ctor = this.classCompiler.CompileCtor(node);
+    return ctor;
+  }
+}
--- packages/compiler/src/Compilers/index.ts
+++ packages/compiler/src/Compilers/index.ts
@@ -1,2 +1,3 @@
 export * from "./Class";
+export * from "./Drivers";
 export * from "./Node";
