6af9a5efbdcbcd258e9942390908c04531f672e0
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-05-25 20:18:13 -0400

    test(core/-Base): UniNodeTypeBase

--- packages/core/src/Nodes/Kinds/-Base.test.ts
+++ packages/core/src/Nodes/Kinds/-Base.test.ts
@@ -1,6 +1,19 @@
 import { describe, expect, it } from "vitest";
 import { IUniNodeBase, UniNodeBase } from "./-Base";
 
+const fixtures = {
+  UniNodeTypeBase: {
+    defaultValues: {
+      kind: "type",
+      type: "unknown",
+      nullable: false,
+      required: true,
+      description: "",
+      default: undefined,
+    },
+  },
+};
+
 describe("UniNodeBase", () => {
   it("should return default values when no options provided", () => {
     expect(UniNodeBase()).toEqual({ kind: "base" });
@@ -21,5 +34,44 @@ describe("UniNodeBase", () => {
     };
 
     expect(UniNodeBase(<NewType>{ kind: "custom", x: true })).toEqual({ kind: "custom", x: true });
+  });
+});
+
+describe("UniNodeTypeBase", () => {
+  it("should return default values when no options provided", () => {
+    expect(UniNodeTypeBase()).toEqual(fixtures.UniNodeTypeBase.defaultValues);
+    expect(UniNodeTypeBase({})).toEqual(fixtures.UniNodeTypeBase.defaultValues);
+  });
+
+  it("should return default values with extra provided values", () => {
+    type NewType = IUniNodeTypeBase & {
+      x: true;
+    };
+
+    expect(
+      UniNodeTypeBase(<NewType>{
+        x: true,
+      }),
+    ).toEqual({
+      ...fixtures.UniNodeTypeBase.defaultValues,
+      x: true,
+    });
+  });
+
+  it("should override default values", () => {
+    type NewType = IUniNodeTypeBase & {
+      x: true;
+    };
+
+    expect(
+      UniNodeTypeBase(<NewType>{
+        type: "custom",
+        x: true,
+      }),
+    ).toEqual({
+      ...fixtures.UniNodeTypeBase.defaultValues,
+      type: "custom",
+      x: true,
+    });
   });
 });
