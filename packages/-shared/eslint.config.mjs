2d0dcc53ade94f40126ca15129b4b5024162865e
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-05-28 15:05:54 -0400

    chore(-shared): no-import-type-side-effects

--- packages/-shared/eslint.config.mjs
+++ packages/-shared/eslint.config.mjs
@@ -7,11 +7,25 @@ const sharedEslintConfig = tseslint.config({
   extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
 
   rules: {
+    "@typescript-eslint/no-import-type-side-effects": "error",
+    "@typescript-eslint/consistent-type-imports": ["error", { fixStyle: "separate-type-imports" }],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "no-empty": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
   },
 });
 
