c6c3a911538f80a90204202eb2294a54fe419eb3
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-05-31 09:05:28 -0400

    feat(compiler/CompileNode): extends NodeVisitor

--- packages/compiler/src/Compilers/Node/CompileNode.ts
+++ packages/compiler/src/Compilers/Node/CompileNode.ts
@@ -1,85 +1,17 @@
-import type {
-  IUniNode,
-  IUniNodeProvider,
-  IUniNodeType,
-  IUniNodeTypeArray,
-  IUniNodeTypeBoolean,
-  IUniNodeTypeFile,
-  IUniNodeTypeInteger,
-  IUniNodeTypeObject,
-  IUniNodeTypeString,
-  IUniNodeView,
-} from "@unispec/core";
+import type { IUniNodeType } from "@unispec/core";
 import type { UniRepository } from "../../Repository";
+import { NodeVisitor } from "../../Visitors";
 import { CompileClass } from "../Class";
 
-export class CompileNode {
+export class CompileNode extends NodeVisitor {
   constructor(
     readonly repository: UniRepository,
     readonly classCompiler: CompileClass = new CompileClass(),
-  ) {}
-
-  HandleTypeString(_: IUniNodeTypeString): unknown {
-    return null;
+  ) {
+    super(false);
   }
 
-  HandleTypeInteger(_: IUniNodeTypeInteger): unknown {
-    return null;
-  }
-
-  HandleTypeBoolean(_: IUniNodeTypeBoolean): unknown {
-    return null;
-  }
-
-  HandleTypeArray(_: IUniNodeTypeArray): unknown {
-    return null;
-  }
-
-  HandleTypeFile(_: IUniNodeTypeFile): unknown {
-    return null;
-  }
-
-  HandleTypeObject(_: IUniNodeTypeObject): unknown {
-    return null;
-  }
-
-  HandleType(node: IUniNodeType): unknown {
-    switch (node.type) {
-      case "string": {
-        return this.HandleTypeString(node);
-      }
-
-      case "integer": {
-        return this.HandleTypeInteger(node);
-      }
-
-      case "boolean": {
-        return this.HandleTypeBoolean(node);
-      }
-
-      case "array": {
-        return this.HandleTypeArray(node);
-      }
-
-      case "reference": {
-        return this.HandleTypeReference(node);
-      }
-
-      case "file": {
-        return this.HandleTypeFile(node);
-      }
-
-      case "object": {
-        return this.HandleTypeObject(node);
-      }
-
-      default: {
-        return null;
-      }
-    }
-  }
-
-  HandleView(_: IUniNodeView): unknown {
+  override OnUnhandled() {
     return null;
   }
 
@@ -90,46 +22,6 @@ export class CompileNode {
       return this.Handle(dereferenced);
     }
 
-    return null;
-  }
-
-  HandleProvider(_: IUniNodeProvider) {
-    return null;
-  }
-
-  HandleDeclarator(_: IUniNode): unknown {
-    return null;
-  }
-
-  HandleOperation(_: IUniNode): unknown {
-    return null;
-  }
-
-  Handle(node: IUniNode): unknown {
-    switch (node.kind) {
-      case "type": {
-        return this.HandleType(node);
-      }
-
-      case "view": {
-        return this.HandleView(node);
-      }
-
-      case "provider": {
-        return this.HandleProvider(node);
-      }
-
-      case "declarator": {
-        return this.HandleDeclarator(node);
-      }
-
-      case "operation": {
-        return this.HandleOperation(node);
-      }
-
-      default: {
-        return null;
-      }
-    }
+    return this.OnUnhandled();
   }
 }
