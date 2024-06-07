5b4e925e1ac85d0cfb2014a8fd41b8780b991d8f
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-06-07 17:30:37 -0400

    feat(ast/npm/builder/utils/simple): SimpleCheck: Boolean check

--- packages/ast/integrations/npm/builder/src/utils/simple.ts
+++ packages/ast/integrations/npm/builder/src/utils/simple.ts
@@ -11,6 +11,6 @@ export const SimpleBuilder = <T extends IUniNodeBase, S extends ISchema<any, any
 export const SimpleCheck = <T extends IUniNodeBase, S extends ISchema<any, any, any, any> = ISchema<any, any, any, any>>(schema: S) => {
   return (data?: any): data is T => {
     const casted = <AnySchema>(<any>schema);
-    return casted.isValidSync(data, { strict: true });
+    return Boolean(data) && casted.isValidSync(data, { strict: true });
   };
 };
