4ce1cd00afa3aafbebff836e05f981158a24ceee
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-06-08 11:31:49 -0400

    fix(drivers/npm/nest/swagger/compile-type): handle view and references

--- packages/drivers/integrations/npm/nestjs/driver-nestjs/src/Swagger/CompileNodeSwaggerType.ts
+++ packages/drivers/integrations/npm/nestjs/driver-nestjs/src/Swagger/CompileNodeSwaggerType.ts
@@ -1,5 +1,6 @@
 import type { SchemaObject } from "@nestjs/swagger/dist/interfaces/open-api-spec.interface";
-import type { IUniNode, IUniNodeTypeArray, IUniNodeTypeString } from "@unispec/ast-types";
+import { CheckType, CheckView } from "@unispec/ast-builder";
+import type { IUniNode, IUniNodeType, IUniNodeTypeArray, IUniNodeTypeString, IUniNodeView } from "@unispec/ast-types";
 import { CompileNode } from "@unispec/ast-utils";
 
 export type ICompiledNodeSwaggerType =
@@ -7,7 +8,8 @@ export type ICompiledNodeSwaggerType =
   | {
       type?: any | [any] | string;
       isArray?: boolean;
-    };
+    }
+  | any;
 
 export class CompileNodeSwaggerType extends CompileNode {
   HandleTypeString(node: IUniNodeTypeString): ICompiledNodeSwaggerType {
@@ -51,6 +53,49 @@ export class CompileNodeSwaggerType extends CompileNode {
     return {
       ...nested,
       isArray: true,
+    };
+  }
+
+  HandleTypeReference(node: IUniNodeType, context?: any) {
+    const dereferenced = this.repository.GetRealTarget(node);
+
+    if (dereferenced) {
+      if (CheckType(dereferenced)) {
+        return this.Handle(
+          {
+            ...dereferenced,
+            required: node.required,
+            nullable: node.nullable,
+            description: node.description,
+          },
+          context,
+        );
+      } else if (CheckView(dereferenced) && CheckType(dereferenced.type)) {
+        return this.Handle(
+          {
+            ...dereferenced,
+            type: {
+              ...dereferenced.type,
+              required: node.required,
+              nullable: node.nullable,
+              description: node.description,
+            },
+          },
+          context,
+        );
+      } else {
+        return this.Handle(dereferenced, context);
+      }
+    }
+
+    return this.OnUnhandled();
+  }
+
+  HandleView(node: IUniNodeView, meta?: Record<string, any>): ICompiledNodeSwaggerType {
+    const ctor = this.classCompiler.CompileCtor(node, null, meta);
+
+    return {
+      type: () => ctor,
     };
   }
 
